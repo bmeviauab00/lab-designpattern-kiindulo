f5be9176ce8a58787b826fdade63e57686dbbd87
Tóth Tibor <toth.tibor@autsoft.hu>
2022-12-12 10:34:12 +0100

    Add project files.

---
+++ DesignPatternApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesignPatternApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            // ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // A legjobb megközelítés a SystemAware lenne (alapértelmezett), de azzal nem jól skálázza
+            // a rajzokat nagy DPI-n.
+            Application.SetHighDpiMode(HighDpiMode.DpiUnawareGdiScaled);
+
+            var mainForm = new MainForm();
+            Application.Run(mainForm);
+        }
+    }
+}
